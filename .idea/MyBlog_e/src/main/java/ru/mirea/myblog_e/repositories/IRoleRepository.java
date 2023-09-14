15d500a80534ff96fa94e93e3e021f46e45f6848
ilya <ilya201555555@gmail.com>
2023-09-15 01:58:22 +0300

    Project5

--- .idea/MyBlog_e/src/main/java/ru/mirea/myblog_e/repositories/IRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.mirea.myblog_e.repositories;
-
-import ru.mirea.myblog_e.models.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IRoleRepository extends JpaRepository<Role, Long> {
-}
--- .idea/MyBlog_e/src/main/resources/dbinfo/users.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO users (email, password, username) VALUES ('', '$2a$12$HGqK6f31VmeUrphTaw1Ih.9HQsinJ0MoNaAJnI6acOr1Z4iPVIzVC', 'admin');
